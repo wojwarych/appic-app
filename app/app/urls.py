b2d89077d377e9e3046e017ac6b669b99a75c835
wojwarych <woj.warych@gmail.com>
2022-11-24 13:49:37 +0100

    added REST framework; added endpoints for create and update events

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from events.views import EventViewSet
+
+router = routers.SimpleRouter()
+router.register(
+    r'events',
+    EventViewSet,
+    basename="Event",
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include((router.urls, 'api')))
 ]
--- app/events/views.py
+++ app/events/views.py
@@ -1,3 +1,14 @@
+from rest_framework import mixins, viewsets
 from django.shortcuts import render
 
+from .models import Event
+from .serializers import EventSerializer
+
 # Create your views here.
+class EventViewSet(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
